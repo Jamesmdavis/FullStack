71af49018432acc69eeceead7727c4513745b940
James Davis <Jamesmdavis95@gmail.com>
2020-02-04 14:46:23 -0800

    rendering a file for homepage GET request

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const app = express();
+const path = require('path');
 const PORT = process.env.PORT || 5000;
 
 app.get('/', (req, res) => {
-    res.send('Hello World');
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
 app.listen(PORT, () => {
