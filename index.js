9a9d60313f71376532426ef25f8d4352bba38da3
James Davis <Jamesmdavis95@gmail.com>
2020-02-04 15:30:24 -0800

    index.js

--- index.js
+++ index.js
@@ -3,9 +3,7 @@ const app = express();
 const path = require('path');
 const PORT = process.env.PORT || 5000;
 
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'index.html'));
-});
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.listen(PORT, () => {
     console.log(`Server listening on port ${PORT}`);
